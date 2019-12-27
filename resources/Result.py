8ab1922e712a361d207709679c6a1d86b1205687
Kristina <kristina.tanya@gmail.com>
2019-12-26 17:06:53 -0800

    set up google cloud nlp, tested out authentication and response

--- resources/Result.py
+++ resources/Result.py
@@ -1,5 +1,26 @@
 from flask_restful import Resource
 
+# import the google cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+  
+# instantiates a client
+client = language.LanguageServiceClient()
+
+# the text to analyze
+text = u'Hello, world!'
+document = types.Document(
+  content=text,
+  type=enums.Document.Type.PLAIN_TEXT)
+
+# detect the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+# print results
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
 results = [
   {
     "sentence": {
