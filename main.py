e66c0ef4b7707ec51c6adebf70e1d75451e084b4
Kristina <kristina.tanya@gmail.com>
2020-01-03 13:17:06 -0800

    created home resource to test deployment

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from resources.Result import Result
+from resources.Home import Home
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Result, '/api/v1/results/<string:text>')
+api.add_resource(Home, '/api/v1')
 
 if __name__ == '__main__':
     app.run(debug=True)
