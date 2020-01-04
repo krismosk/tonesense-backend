1b3d58f8e3eb3cdb2abeec9884ae67749a21dbee
Kristina <kristina.tanya@gmail.com>
2020-01-04 14:29:38 -0800

    deployed to aws eb

---
+++ application.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from resources.Result import Result
+from resources.Home import Home
+import os
+from dotenv import load_dotenv
+import logging
+
+# import the google cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+logging.basicConfig(filename='/tmp/tmp.log',level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+# load dotenv in the base root
+# APP_ROOT = os.path.join(os.path.dirname(__file__), '..') 
+# dotenv_path = os.path.join(APP_ROOT, '.env')
+# load_dotenv(dotenv_path)
+load_dotenv(verbose=True)
+logging.info(os.environ)
+logging.info(os.getcwd())
+
+logging.warning("foobar")
+logging.warning(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+
+application = api = Flask(__name__)
+
+api = Api(application)
+
+api.add_resource(Result, '/api/v1/results/<string:text>')
+# api.add_resource(Home, '/api/v1')
+api.add_resource(Home, '/')
+
+# logging.warning("foobar test")
+# client = language.LanguageServiceClient()
+# logging.warning("test2")
+# client = language.LanguageServiceClient()
+# logging.warning("test3")
+# client = language.LanguageServiceClient()
+# logging.warning("test4")
+
+if __name__ == '__main__':
+    application.run(debug=True)
--- resources/Result.py
+++ resources/Result.py
@@ -1,17 +1,19 @@
 from flask_restful import Resource, reqparse
 from flask import jsonify
 import json
+import logging
 
 # import the google cloud client library
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-from google.protobuf.json_format import MessageToDict
 
 class Result(Resource):
+  client = None
+  
   def post(self, text):
     text = text
-    testing = True
+    testing = False
     if testing:
       return self.get_mock_sentiment(text)
     else:
@@ -38,32 +40,48 @@ class Result(Resource):
     return response
 
   def get_sentiment(self, text):
-    # instantiates a client
-    client = language.LanguageServiceClient()
-    
-    # the text to analyze
-    analyzed_text = text
-    document = types.Document(
-      content=analyzed_text,
-      language='en',
-      type=enums.Document.Type.PLAIN_TEXT)
-
-    # detect the sentiment of the text
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
-    entity_response = client.analyze_entity_sentiment(document=document, encoding_type='UTF32')
-
-    print('moskalets')
     result = {}
-    result['text'] = text
-    result['score'] = sentiment.score
-    result['magnitude'] = sentiment.magnitude
-    result['entities'] = []
+    try:
+      logging.info("get sentiment")
+      # instantiates a client
+      client = Result.client
+      if client is None:
+        logging.info("client created")
+        Result.client = language.LanguageServiceClient()
+        client = Result.client
+      
+      # the text to analyze
+      analyzed_text = text
+      document = types.Document(
+        content=analyzed_text,
+        language='en',
+        type=enums.Document.Type.PLAIN_TEXT)
+      logging.info("document created: {}".format(document))
 
-    for entity in entity_response.entities:
-      result['entities'].append({'name': entity.name, 'type': entity.type, 'salience': entity.salience})
+      # detect the sentiment of the text
+      sentiment = client.analyze_sentiment(document=document, retry=None, timeout=10.0).document_sentiment
+      logging.info("analyzed sentiment")
 
-    print('result printed:')
-    print(result)
+      entity_response = client.analyze_entity_sentiment(document=document, retry=None, timeout=10.0, encoding_type='UTF32')
+      logging.info("sentiment done")
 
-    response = jsonify(result)
-    return response
+      print('moskalets')
+      result['text'] = text
+      result['score'] = sentiment.score
+      result['magnitude'] = sentiment.magnitude
+      result['entities'] = []
+
+      for entity in entity_response.entities:
+        result['entities'].append({'name': entity.name, 'type': entity.type, 'salience': entity.salience})
+    except:
+      logging.warn("exception")
+      e = sys.exc_info()[0]
+      logging.error(e)
+    finally:
+      print('result printed:')
+      print(result)
+      return jsonify(result)
+    
+
+
+
