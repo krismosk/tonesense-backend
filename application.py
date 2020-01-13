875077188b49108033f14e61d783a2402e80657e
Kristina <kristina.tanya@gmail.com>
2020-01-12 18:06:23 -0800

    create new resource for naive bayes classifier

--- application.py
+++ application.py
@@ -2,22 +2,11 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from resources.Result import Result
 from resources.Home import Home
+from resources.NaiveBayes import NaiveBayes
 import os
 from dotenv import load_dotenv
 import logging
 
-# import the google cloud client library
-# from google.cloud import language
-# from google.cloud.language import enums
-# from google.cloud.language import types
-
-# logging.basicConfig(filename='/tmp/tmp.log',level=logging.DEBUG)
-# logging.getLogger().addHandler(logging.StreamHandler())
-
-# load dotenv in the base root
-# APP_ROOT = os.path.join(os.path.dirname(__file__), '..') 
-# dotenv_path = os.path.join(APP_ROOT, '.env')
-# load_dotenv(dotenv_path)
 load_dotenv(verbose=True)
 logging.info(os.environ)
 logging.info(os.getcwd())
@@ -28,7 +17,8 @@ application = api = Flask(__name__)
 
 api = Api(application)
 
-# api.add_resource(Result, '/api/v1/results/<string:text>')
+api.add_resource(Result, '/api/v1/results/<string:text>')
+api.add_resource(NaiveBayes, '/api/v2/results/<string:text>')
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
